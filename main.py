358af292d663024dc4fe2571a969f9a4d127b536
Vitalii Hoshchenko <viposha@gmail.com>
2022-12-01 15:56:10 +0200

    Start bot with 'start' command

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import telebot
 
+bot = telebot.TeleBot('5924761149:AAErqGWIf0NJ2UboK7OJ4-wXfEwTq-UbAF0')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+	mess = f'Hello, {message.from_user.first_name}'
+	bot.send_message(message.chat.id, mess)
+
+
+bot.polling(none_stop=True)
