1d6718ffdc226e4ef0f51e214db92baa658f401f
Vitalii Hoshchenko <viposha@gmail.com>
2023-01-23 14:14:57 +0200

    modify test schedule file

--- sch_test.py
+++ sch_test.py
@@ -3,12 +3,22 @@ import time
 import schedule
 
 hostname = '188.190.241.223'
+result = [0]
 
-def job():
+def switch():
     response = os.system('ping -c 4 ' + hostname)
-    print(response)
+    result.append(response)
+    print(result)
+    if result[0] == 0 and result[1] == 256:
+        print('Світло вимкнули')
+    elif result[0] == 256 and result[1] == 0:
+        print('Світло ввімкнули')
+    else:
+        print('працює')
+    result.pop(0)
+    print(result)
 
-schedule.every(10).seconds.do(job)
+schedule.every(10).seconds.do(switch)
 
 while True:
     schedule.run_pending()
