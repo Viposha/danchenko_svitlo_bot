ae7ad5a9e41f82546e52d04256c260816f9db228
Vitalii Hoshchenko <viposha@gmail.com>
2023-02-01 13:42:51 +0200

    refactor db client with class

---
+++ db_client.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+
+class DBClient:
+
+	"""Initialisation db with its pathname. If it doesn`t exist - create db """
+
+	def __init__(self, pathname: str):
+		self.conn = None
+		self.pathname = pathname
+
+	""" Create connection to db"""
+
+	def create_conn(self):
+		self.conn = sqlite3.connect(self.pathname, check_same_thread=False)
+
+	"""Create Table in db with query"""
+
+	def create_table(self, query: str):
+		self.conn.execute(query)
+		self.conn.commit()
+
+	"""Execute any query to db"""
+
+	def execute_command(self, query: str, params: tuple):
+		self.conn.execute(query, params)
+		self.conn.commit()
+
+	"""Execute SELECT query and return data"""
+
+	def execute_select_command(self, query: str):
+		cur = self.conn.cursor()
+		cur.execute(query)
+		return cur.fetchall()
+
---
+++ main_2.py
@@ -0,0 +1,127 @@
+import telebot
+import sqlite3
+import os
+from telebot import types
+from apscheduler.schedulers.background import BackgroundScheduler
+from db_client import DBClient
+
+hostname = os.getenv('HOSTNAME')
+token = os.getenv('TOKEN')
+bot = telebot.TeleBot(token)
+pathname = r"/danchenko_svitlo_bot/database/danchenko_svitlo_users.db"
+sched = BackgroundScheduler()
+url = 'https://kyiv.yasno.com.ua/schedule-turn-off-electricity'
+db = DBClient(pathname)
+result = [5]
+
+sql_create_table = """ 
+						CREATE TABLE IF NOT EXISTS Users 
+						(
+						id integer PRIMARY KEY,
+						username text,
+						chat_id integer UNIQUE
+						); 
+					"""
+sql_insert_into_db = """ 
+					INSERT INTO Users(username,chat_id)
+					VALUES(?,?) 
+					"""
+sql_select_chat_id = """SELECT chat_id FROM Users"""
+
+db.create_conn()
+db.create_table(sql_create_table)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	btn1 = types.KeyboardButton("\U0001F50E Зараз є світло?")
+	btn2 = types.KeyboardButton("\U0001F3E0 Графік Данченка 28")
+	btn3 = types.KeyboardButton("\U0001F4CA Графік інша адреса")
+	markup.add(btn1, btn2, btn3)
+	bot.send_message(message.chat.id,
+					 text="Вітаю, {0.first_name}!\nТут ти зможеш дізнатися про наявність світла\n"
+						  "За орієнтир взято будинок по вул. Данченка 28\n"
+						  "Система автоматично за 5 хвилин повідомить про ввімкнення чи вимкнення світла\n"
+						  "Інформацію від енергетика про планові дії\n"
+						  "Та додаткову інформацію по освітленню".format(message.from_user), reply_markup=markup)
+	try:
+		db.execute_command(sql_insert_into_db, extract(message))
+	except sqlite3.IntegrityError as error:
+		print(error)
+
+
+@bot.message_handler(content_types=['text'])
+def get_usr_text(message):
+
+	if message.text == "\U0001F50E Зараз є світло?":
+		if result == [0]:
+			bot.send_message(message.chat.id, '\U0001F7E2 Світло є')
+		elif result == [256]:
+			bot.send_message(message.chat.id, '\U0001F534 Світла нема')
+		else:
+			bot.send_message(message.chat.id, '\U0001F7E0 Наразі невідомо\nЗапитай через 5 хвилин')
+
+	elif message.text == "\U0001F3E0 Графік Данченка 28":
+		bot.send_photo(message.chat.id, open("/danchenko_svitlo_bot/database/graph_28.jpg", 'rb'))
+
+	elif message.text == "\U0001F4CA Графік інша адреса":
+		bot.send_message(message.chat.id, f'Перейди по посиланню\n{url}')
+
+	else:
+		if message.chat.id == 482085376:
+			bot.register_next_step_handler(message, text_from_ivan)  # Чекаю новий message і передаю в функцію text_from_ivan
+		else:
+			bot.send_message(message.chat.id, f'Ця команда недоступна\nПочни роботу з /start')
+
+
+def text_from_ivan(message):
+
+	"""Відправляю повідомлення (Що сказав Іван) всім юзерам з бази даних"""
+
+	data = db.execute_select_command(sql_select_chat_id)
+	for chat_id in data:
+		try:
+			bot.send_message(chat_id[0], message.text)
+		except telebot.apihelper.ApiTelegramException as error:
+			if "Forbidden: bot was blocked by the user" in error.description:
+				sql = f"""DELETE FROM Users WHERE chat_id == {chat_id[0]}"""
+				db.execute_command(sql)
+
+
+def extract(message):
+
+	"""Розпаковка інформації від юзера для подальшого запису в базу.
+	   Відокремлення username та user_id"""
+
+	username = message.from_user.username
+	if username == None:
+		username = 'name:{0.first_name}'.format(message.from_user)
+	user_id = message.from_user.id
+	return username, user_id
+
+
+def switch():
+	"""Пінгує другий роутер і при зміні result[0] на result[0,1] відправляє повідомлення всім з db"""
+	global result
+	response = os.system('ping -c 4 ' + hostname)
+	result.append(response)
+	data = db.execute_select_command(sql_select_chat_id)
+	for chat_id in data:
+		try:
+			if result[0] == 0 and result[1] == 256:
+				bot.send_message(chat_id[0], '\U0001F303 Світло вимкнули')
+			elif result[0] == 256 and result[1] == 0:
+				bot.send_message(chat_id[0], '\U0001F306 Світло ввімкнули')
+			else:
+				pass
+		except telebot.apihelper.ApiTelegramException as error:
+			if "Forbidden: bot was blocked by the user" in error.description:
+				sql = f"""DELETE FROM Users WHERE chat_id == {chat_id[0]}"""
+				db.execute_command(sql)
+	result.pop(0)
+
+
+sched.add_job(switch, 'interval', minutes=3)
+sched.start()
+bot.polling(none_stop=True)
