1836365ce197589510461b8d3aa03a99e6a4c279
Vitalii Hoshchenko <Viposha@gmail.com>
2024-04-06 23:34:12 +0300

    final version

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ from db_client import DBClient
 from datetime import date
 
 hostname = os.getenv('HOSTNAME')
-#hostname = '188.190.241.223'
 token = os.getenv('TOKEN')
-#token = '5924761149:AAErqGWIf0NJ2UboK7OJ4-wXfEwTq-UbAF0'
 bot = telebot.TeleBot(token)
 pathname = r"database/danchenko_svitlo_users.db"
 sched = BackgroundScheduler()
